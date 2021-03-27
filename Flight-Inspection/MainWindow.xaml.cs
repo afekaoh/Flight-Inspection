99158700af2ed479f5b0b35c69e7cb309d41c278
Adam Shapira <afekaoh@gmail.com>
2021-03-27 10:08:52 +0300

    added control directory and moved FG_Control to it

--- Flight-Inspection/MainWindow.xaml.cs
+++ Flight-Inspection/MainWindow.xaml.cs
@@ -25,20 +25,5 @@ namespace Flight_Inspection
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var procPath = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\FlightGear 2020.3.6";
-            Process proc = new Process();
-            proc.StartInfo.FileName = "C:\\Program Files\\FlightGear 2020.3.6\\bin\\fgfs.exe";
-            proc.StartInfo.WorkingDirectory = "C:\\Program Files\\FlightGear 2020.3.6\\bin";
-            proc.StartInfo.Arguments = "--generic=socket,in,10,127.0.0.1,5400,tcp,playback_small --fdm=null";
-            proc.Start();
-        }
-
-        private void b1_KeyUp(object sender, KeyEventArgs e)
-        {
-            b1.
-        }
     }
 }
