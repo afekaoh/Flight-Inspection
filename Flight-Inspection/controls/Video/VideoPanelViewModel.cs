e161d4154449a5596b2e32cf9cb455c4d09b0fa0
RabkinMichelle <michellerabkin223@gmail.com>
2021-04-05 18:22:19 +0300

    added video panel module

---
+++ Flight-Inspection/controls/Video/VideoPanelViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Inspection.controls.Video
+{
+    class VideoPanelViewModel
+    {
+        private int maxSlider;
+
+        public int MaxSlider
+        {
+            get {
+                return maxSlider; 
+            }
+            set { maxSlider = value; }
+        }
+
+
+        private int currentTime;
+
+        public int CurrentTime
+        {
+            get { return currentTime;
+            }
+            set { currentTime = value; }
+        }
+
+    }
+
+}
+
+
