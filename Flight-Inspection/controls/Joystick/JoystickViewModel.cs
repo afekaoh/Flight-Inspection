a3973804293310969a8ae39f619a04ba16ce4dce
oz rigler <ozzri28@gmail.com>
2021-04-07 15:22:53 +0300

    open joystick moudle

---
+++ Flight-Inspection/controls/Joystick/JoystickViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Inspection.controls.Joystick
+{
+    class JoystickViewModel : IControlViewModel
+    {
+        public override void SetSettings(SettingsArgs settingsArgs)
+        {
+        }
+    }
+}
